e8973c40a1d13739899d695758f01cc6df80a934
jotaRacer <juan.ugaz@mail.udp.cl>
2025-06-09 17:09:19 -0400

    Lineas basura Eliminadas

--- src/util/AsignadorCategoria.java
+++ src/util/AsignadorCategoria.java
@@ -1,15 +1,13 @@
 package util;
 
-import model.Paciente;
-
 public class AsignadorCategoria {
     
     public static int asignarCategoriaAleatoria() {
-        double probabilidad = Math.random() * 100; // Genera un número entre 0 y 100
+        double probabilidad = Math.random() * 100; // Genera un número entre 0 y 100.
 
-        // Asigna la categoría de acuerdo con las probabilidades
+        // Asigna la categoría de acuerdo con las probabilidades.
         if (probabilidad < 10) {
-            return 1; // C1: Emergencia Vital (10%)
+            return 1;
         } else if (probabilidad < 25) {
             return 2;
         } else if (probabilidad < 43) {
